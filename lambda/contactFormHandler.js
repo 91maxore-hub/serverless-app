0c4a470557049c084ebfc230d0b93e6dc049e1c1
Max Oredson <91maxore@gafe.molndal.se>
2025-11-17 10:52:42 +0100

    CI/CD Pipeline

--- lambda/contactFormHandler.js
+++ lambda/contactFormHandler.js
@@ -3,14 +3,18 @@ const crypto = require("crypto");
 
 const db = new DynamoDBClient({ region: "eu-west-1" });
 
+// Ange CloudFront URL som CORS origin
+const ALLOWED_ORIGIN = "https://dttru2ys9r817.cloudfront.net";
+
 exports.handler = async (event) => {
   console.log("Incoming event:", JSON.stringify(event));
 
+  // Hantera preflight (OPTIONS) request
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 204,
       headers: {
-        "Access-Control-Allow-Origin": "https://dttru2ys9r817.cloudfront.net",
+        "Access-Control-Allow-Origin": ALLOWED_ORIGIN,
         "Access-Control-Allow-Methods": "POST,OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type",
       },
@@ -18,6 +22,7 @@ exports.handler = async (event) => {
     };
   }
 
+  // Parsning av inkommande JSON
   let data;
   try {
     data = JSON.parse(event.body || "{}");
@@ -25,7 +30,7 @@ exports.handler = async (event) => {
     console.error("JSON parse error:", e);
     return {
       statusCode: 400,
-      headers: { "Access-Control-Allow-Origin": "https://dttru2ys9r817.cloudfront.net" },
+      headers: { "Access-Control-Allow-Origin": ALLOWED_ORIGIN },
       body: JSON.stringify({ error: "Invalid JSON in request" })
     };
   }
@@ -44,14 +49,15 @@ exports.handler = async (event) => {
   };
 
   try {
-    console.log("Trying to write to Dynamo:", params);
+    console.log("Trying to write to DynamoDB:", params);
     await db.send(new PutItemCommand(params));
     console.log("Write SUCCESS");
 
+    // Returnera alltid JSON med korrekta CORS headers
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "https://dttru2ys9r817.cloudfront.net",
+        "Access-Control-Allow-Origin": ALLOWED_ORIGIN,
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "POST,OPTIONS",
       },
@@ -68,7 +74,9 @@ exports.handler = async (event) => {
     return {
       statusCode: 500,
       headers: {
-        "Access-Control-Allow-Origin": "https://dttru2ys9r817.cloudfront.net"
+        "Access-Control-Allow-Origin": ALLOWED_ORIGIN,
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST,OPTIONS",
       },
       body: JSON.stringify({ error: "Could not save message", details: err.message })
     };
